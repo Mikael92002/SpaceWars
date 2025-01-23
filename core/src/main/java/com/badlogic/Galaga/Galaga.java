4a3554d67240ed3b2d4a55123ca52812ace9d1e9
Mikael92002 <Mikael92002@users.noreply.github.com>
2025-01-22 18:03:15 -0600

    Initial commit

---
+++ core/src/main/java/com/badlogic/Galaga/Galaga.java
@@ -0,0 +1,37 @@
+package com.badlogic.Galaga;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Galaga extends Game {
+    public SpriteBatch batch;
+    public Texture image;
+    FreeTypeFontGenerator generator;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
